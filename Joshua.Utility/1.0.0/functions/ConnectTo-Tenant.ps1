eb3f0843b30352da337bd336d45b921a69ffe10a
Joshua Saputil <joshuasaputil97@hotmail.com>
2023-03-19 21:06:33 +0100

    Added new cmdlet for tenant automation

---
+++ Joshua.Utility/1.0.0/functions/ConnectTo-Tenant.ps1
@@ -0,0 +1,52 @@
+function ConnectTo-Tenant {
+  [Alias("ctt")]
+  param(
+    [Parameter()]
+    [String]
+    $SetPath
+    ,
+    [Parameter()]
+    [String]
+    $URL
+  )
+  
+  if ($PSBoundParameters.ContainsKey("SetPath")){
+    if ($SetPath -notlike "*ctt-*.conf"){
+      throw
+    }
+    [System.Environment]::SetEnvironmentVariable('TENANT_CONF_PATH', $SetPath, 'User') 
+    Write-Host "Set path for $SetPath"
+    Write-Host "Please restart powershell session to take effect"
+  }
+  
+  try 
+  {
+    $Config = Get-Content -Path $env:TENANT_CONF_PATH
+  }
+  catch 
+  {
+    Write-Host "No set values for: $env:TENANT_CONF_PATH"
+    Write-Host "Please use filepath and set to config file"
+    Throw
+  }
+  
+  $CLIENT_ID = ($Config | Where-Object {$_ -like "CLIENT_ID*"}).Split("=")[1]
+  $TENANT_ID = ($Config | Where-Object {$_ -like "TENANT_ID*"}).Split("=")[1]
+  $KEY_PATH = ($Config | Where-Object {$_ -like "KEY_PATH*"}).Split("=")[1]
+  
+  if ($PSBoundParameters.ContainsKey("URL")){
+    try
+    {
+      Write-Host "Attemping to connect to site: " -NoNewline
+      Write-Host "$URL" -ForegroundColor Green 
+      Connect-PnPOnline -Url $URL -ClientId $CLIENT_ID -Tenant $TENANT_ID -CertificatePath $KEY_PATH
+      Write-Host "Successfully connected to site: " -NoNewline
+      Write-Host "$URL" -ForegroundColor Green 
+    }
+    catch 
+    {
+      Write-Host "Could not connect"
+      throw
+    }
+  }
+}
--- Joshua.Utility/1.0.0/functions/Get-God.ps1
+++
@@ -1,3 +0,0 @@
-function Get-God ($Phrase) {
-  Write-Host "I just wanted to thank God! $Phrase"
-}
