67bad5154d7923e55892b72fc9420ba6cb495567
Joshua Ablian Saputil <joshuasaputil97@hotmail.com>
2023-04-04 09:33:48 +0200

    New cmdlet pick-pnplistitem

---
+++ 1.0.0/functions/Pick-PNPListItem.ps1
@@ -0,0 +1,12 @@
+function Pick-PNPListItem {
+  $site = $(Get-PnPWeb).Url
+  Write-Host "Pick a list to browse from site: " -NoNewline
+  Write-Host "$site" -ForegroundColor Green
+
+  [void]($a = Get-PnPList)
+  $list = $a.Title | Invoke-Fzf # Choose a site list
+  Write-Host "Selected list: " -NoNewline
+  Write-Host "$list " -ForegroundColor Green -NoNewline
+  Write-Host "from site: " -NoNewline
+  Write-Host "$site" -ForegroundColor Green
+}
